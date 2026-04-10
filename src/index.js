b47bd2a263e657b4b421f1256b91070be04d077c
Yaroslav <work.freshcode@gmail.com>
2026-04-10 16:57:49 +0300

    Виправив шлях до скрипта

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-loop: for(let i = 2; i <= 100; i++) {
-    for(let j = 2; j < i; j++) {
-        if(i % j === 0) {
-            continue loop;
-        }
+loop: for (let i = 2; i <= 100; i++) {
+    for (let j = 2; j < i; j++) {
+        if (i % j === 0) continue loop;
     }
-    console.log(i)
+    console.log(i);
 }
